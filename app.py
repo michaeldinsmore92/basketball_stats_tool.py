16a3b07ab2612db9814676d999c2affd835ce653
Mikey Dinsmore <52056199+mikeydinsmore@users.noreply.github.com>
2019-07-17 20:19:25 -0700

    UPDATE --- 7/17/19 --- 8:19 PM
    UPDATE

--- app.py
+++ app.py
@@ -1,104 +1,152 @@
+#  Imported libraries/files
 import constants
-import random
 import sys
+from copy import deepcopy
 
 
-PLAYERS = constants.PLAYERS
-TEAMS = constants.TEAMS
-LENGTH = len(PLAYERS)
-
-
+# Global variables
+players = deepcopy(constants.PLAYERS) #  copied from 'constants.py'
+teams = deepcopy(constants.TEAMS) #  copied from 'constants.py'
 experienced = []
 inexperienced = []
-for player in PLAYERS:
-    if player['experience'] == 'YES':
-        experienced.append(player['name'])
-    else:
-        inexperienced.append(player['name'])
-        
-        
-guardians = []
-for player in PLAYERS:
-    guardians.append(player['guardians'])
-    
-    
-heights = []
-for player in PLAYERS:
-    heights.append(int(player['height'][:2]))
-    
+panthers = []
+bandits = []
+warriors = []
 
+
+#  Creates function to clean the data copied from 'constants.py'
+def clean_data():
+      #  For each player, convert the string value of 'experience' to boolean value 
+      #  and add to new list (experienced / inexperienced)
+      for player in players:
+            #  If the exprience value is 'YES', convert to True
+            if player['experience'] == 'YES':
+                  player['experience'] = True
+                  #  Add to experienced player's list
+                  experienced.append(player)
+            #  If the exprience value is 'NO', convert to False
+            elif player['experience'] == 'NO':
+                  player['experience'] = False
+                  #  Add to inexperienced player's list
+                  inexperienced.append(player)
+      #  For each player, convert the string value of 'height' to integer
+      for player in players:
+            #  Slice the string value to only gather numbers
+            player['height'] = player['height'][:2]
+            #  Convert the string value to integer
+            int(player['height'])
+            
+
+#  Creates function to assign players and 
+#  evenly balance experienced/inexperienced among teams            
+def create_teams(exp, inexp):
+      panthers.append(exp[:3] + inexp[:3])
+      bandits.append(exp[3:6] + inexp[3:6])
+      warriors.append(exp[6:] + inexp[6:])
+
+
+#  Creates a function to display the user selected team
+def display_team(team):
+      #  Clean display
+      print("-" * 10)
+      print("Total Players: 6\n\n"
+            "Players on Team:")
+      #  For each player in user selected team, print each name
+      i = 0
+      while i < len(team):
+            for player in team[i]:
+                  if player['experience'] == True:
+                        print(f"{player['name']} (Experienced)")
+                  else:
+                      print(f"{player['name']} (Inexperienced)")
+            i += 1
+      print("\n")
+      #  For each player in user selected team, print each player's guardian(s)
+      print("Guardians: ")
+      i = 0
+      while i < len(team):
+            for player in team[i]:
+                  print(f"{player['guardians']} ({player['name']})")
+            i += 1
+      print("\n")
+      #  Print the average height of the team in inches 
+      #  (sum of team height in inches / num of players)
+      sum = 0
+      i = 0
+      while i < len(team):
+            for player in team[i]:
+                  sum += int(player['height'])
+            i += 1
+      average = int(sum / 6)
+      print(f"Average Team Height: {average} inches")
+      print("\n")
+
+
+#  Creates a function to prompt the user and display stats
 def display_stats():
+      #  Display main menu
       print("BASKETBALL TEAM STATS TOOL:\n\n"
             "---MENU---\n\n"
             "Here are your choices:\n"
             "1) Display Team Stats\n"
             "2) Quit\n")
-      user_option = input("Enter an option: ")
+      #  Prompt the user to enter an option (1/2)
+      try:
+            user_option = input("Enter an option: ")
+      #  Catch potential input error
+      except EOFError as err:
+            pass
+      #  Try to convert input into integer
       try:
             user_option = int(user_option)
+      #  Catch potential input error
       except ValueError as err:
             print("\nOops! Please enter either 1 or 2...\n")
             display_stats()
+      except UnboundLocalError as err:
+            pass
       else:      
+            #  If the user elects to quit, exit program
             if user_option == 2:
                   print("Come back anytime! Goodbye!")
                   sys.exit()
+            #  Otherwise, display team names
             else:
-                  print(f"1) {TEAMS[0]}\n"
-                        f"2) {TEAMS[1]}\n"
-                        f"3) {TEAMS[2]}\n")
+                  print(f"1) {teams[0]}\n"
+                        f"2) {teams[1]}\n"
+                        f"3) {teams[2]}\n")
+            #  Try to convert input into integer
             try:
                   user_option2 = int(input("Enter an option: "))
+            #  Catch potential input errors
             except ValueError as err:
                   print("\nOops! Please enter 1, 2, or 3...\n")
                   display_stats()
+            except EOFError as err:
+                  pass
             else:
+                  #  If user selects 1, display team[0] "Panthers" stats
                   if user_option2 == 1:
-                        print(f"Team: '{TEAMS[0]}' Stats")
-                        print("-" * 10)
-                        print("Total Players: 6\n\n"
-                              "Players on Team:")
-                        print("Experienced: " + ", ".join(experienced[:3]))
-                        print("Inexperienced: " + ", ".join(inexperienced[:3]))
-                        print("\n")
-                        print("Guardians:")
-                        print(", ".join(guardians[:6]))
-                        print("\n")
-                        average_heights = int(sum(heights[:6]) / 3)
-                        print("Average Height: " + f"{average_heights} inches")
-                        print("\n")
+                        print(f"Team: '{teams[0]}' Stats")
+                        display_team(panthers)
                         display_stats()
+                  #  If user selects 2, display team[1] "Bandits" stats
                   elif user_option2 == 2:
-                        print(f"Team: '{TEAMS[1]}' Stats")
-                        print("-" * 10)
-                        print("Total Players: 6\n\n"
-                              "Players on Team:")
-                        print("Experienced: " + ", ".join(experienced[3:6]))
-                        print("Inexperienced: " + ", ".join(inexperienced[3:6]))
-                        print("\n")
-                        print("Guardians:")
-                        print(", ".join(guardians[6:12]))
-                        print("\n")
-                        average_heights = int(sum(heights[6:12]) / 3)
-                        print("Average Height: " + f"{average_heights} inches")
-                        print("\n")
+                        print(f"Team: '{teams[1]}' Stats")
+                        display_team(bandits)
                         display_stats()
+                  #  If user selects 3, display team[2] "Warriors" stats
                   elif user_option2 == 3:
-                        print(f"Team: '{TEAMS[2]}' Stats")
-                        print("-" * 10)
-                        print("Total Players: 6\n\n"
-                              "Players on Team:")
-                        print("Experienced: " + ", ".join(experienced[6:]))
-                        print("Inexperienced: " + ", ".join(inexperienced[6:]))
-                        print("\n")
-                        print("Guardians:")
-                        print(", ".join(guardians[12:]))
-                        print("\n")
-                        average_heights = int(sum(heights[12:]) / 3)
-                        print("Average Height: " + f"{average_heights} inches")
-                        print("\n")
+                        print(f"Team: '{teams[2]}' Stats")
+                        display_team(warriors)
                         display_stats()
 
 
+# Proper use of dunder __main__ ? :D
 if __name__ == '__main__':
+      #  Cleans the data copied from 'constants.py'
+      clean_data()
+      #  Creates 3 equally balanced teams
+      create_teams(experienced, inexperienced)
+      #  Start program
       display_stats()
